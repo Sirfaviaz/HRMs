5c07973ce1358ccdfe7a94c09f8ed86c49b3942c
Favaz Mohammed <143575761+Sirfaviaz@users.noreply.github.com>
2024-11-01 20:05:18 +0530

    Update settings.py

--- hrm_backend/settings.py
+++ hrm_backend/settings.py
@@ -238,14 +238,6 @@ STATIC_URL = "static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "ecombrocom@gmail.com"
-EMAIL_HOST_PASSWORD = "smja ihld snwn nrlr"
 
 
 
